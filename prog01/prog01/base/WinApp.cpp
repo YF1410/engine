ebc94a22a8d344c45188ea231e566d5cc333c058
YutaFujinami <sakure3346@yahoo.co.jp>
2022-04-11 11:55:57 +0900

    ファイル分け、verアップデート

--- prog01/prog01/base/WinApp.cpp
+++ prog01/prog01/base/WinApp.cpp
@@ -3,7 +3,7 @@
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-const wchar_t WinApp::windowClassName[] = L"LE2B_26_フジナミユウタ";
+const wchar_t WinApp::windowClassName[] = L"LE3A_20_フジナミユウタ";
 
 LRESULT WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
